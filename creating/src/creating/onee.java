9312a4aa5e60c34b511b1af4c9d6c75a123164b3
jaisandeep <ajsy7009@gmail.com>
2021-09-11 14:12:45 +0530

    hii man

---
+++ creating/src/creating/onee.java
@@ -0,0 +1,13 @@
+package creating;
+
+public class onee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("welocome to git");
+		
+	}
+
+}
